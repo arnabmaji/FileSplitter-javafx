f307683eadcca78e113606fd94a8cc980b0c3638
Arnab Maji <arnabmaji1999@gmail.com>
2020-03-22 18:19:02 +0530

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module io.github.arnabmaji19.filesplitter {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens io.github.arnabmaji19.filesplitter to javafx.fxml;
+    exports io.github.arnabmaji19.filesplitter;
+}

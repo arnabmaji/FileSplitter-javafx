34e79fb33dc42e7124a7c2fa923c8e3daa6e242c
Arnab Maji <arnabmaji1999@gmail.com>
2020-03-23 11:25:17 +0530

    add actions to file splitter window

--- src/main/java/io/github/arnabmaji19/filesplitter/controller/FileSplitterController.java
+++ src/main/java/io/github/arnabmaji19/filesplitter/controller/FileSplitterController.java
@@ -1,4 +1,51 @@
 package io.github.arnabmaji19.filesplitter.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class FileSplitterController {
+    @FXML
+    private TextField filePathTextField;
+    @FXML
+    private Spinner<Integer> maxPartitionsSpinner;
+
+    private File file;
+    private FileChooser fileChooser;
+
+    public FileSplitterController() {
+        this.fileChooser = new FileChooser();
+    }
+
+    @FXML
+    private void chooseFile() {
+        var selectedFile = fileChooser.showOpenDialog(new Stage());  // open file chooser window
+
+        if (selectedFile != null) {
+            file = selectedFile;
+            // update file path in the file path text field
+            filePathTextField.setText(file.getAbsolutePath());
+        }
+    }
+
+    @FXML
+    private void generatePartitions() {
+        if (file == null) { // if user has not selected any file
+            // show error dialog
+            var errorAlert = new Alert(
+                    Alert.AlertType.ERROR,
+                    "No File Chosen",
+                    ButtonType.OK
+            );
+            errorAlert.showAndWait();
+            return;
+        }
+        int maxPartitions = maxPartitionsSpinner.getValue();  // get number of partitions
+    }
 }

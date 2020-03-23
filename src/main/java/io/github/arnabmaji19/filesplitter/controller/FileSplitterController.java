804a0301b2ed6aaf52f1bd0c2d77d4fa0969ee43
Arnab Maji <arnabmaji1999@gmail.com>
2020-03-23 11:02:28 +0530

    create file splitter layout

---
+++ src/main/java/io/github/arnabmaji19/filesplitter/controller/FileSplitterController.java
@@ -0,0 +1,4 @@
+package io.github.arnabmaji19.filesplitter.controller;
+
+public class FileSplitterController {
+}
--- src/main/java/io/github/arnabmaji19/filesplitter/controller/PrimaryController.java
+++ src/main/java/io/github/arnabmaji19/filesplitter/controller/PrimaryController.java
@@ -1,13 +1,30 @@
 package io.github.arnabmaji19.filesplitter.controller;
 
+import io.github.arnabmaji19.filesplitter.App;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PrimaryController {
 
+    private Stage stage;
+    private Scene scene;
+
+    public PrimaryController() {
+        this.stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setResizable(false);
+    }
 
     @FXML
-    private void viewFileSplitter() {
-        System.out.println("File Splitter");
+    private void viewFileSplitter() throws IOException {
+        this.scene = new Scene(App.loadFXML("file_splitter"), 600, 120);
+        stage.setScene(scene);
+        stage.setTitle("File Splitter");
+        stage.show();
     }
 
     @FXML

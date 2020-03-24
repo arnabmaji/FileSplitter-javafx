b4b6f7a92dd32cf0e27b425391a34a9a85708abf
Arnab Maji <arnabmaji1999@gmail.com>
2020-03-24 11:22:47 +0530

    create FileMerger class

---
+++ src/main/java/io/github/arnabmaji19/filesplitter/util/FileMerger.java
@@ -0,0 +1,90 @@
+package io.github.arnabmaji19.filesplitter.util;
+
+import java.io.*;
+
+public class FileMerger extends Thread {
+
+    private static final int FILE_NAME_LINE_NUMBER = 2;
+    private static final int PARTITIONS_NAME_START_NUMBER = 3;
+
+    private File metaFile;
+    private String workingDirectory;
+    private String outputFileName;
+    private ProgressListener progressListener;
+
+    public FileMerger(File metaFile) {
+        this.metaFile = metaFile;
+        this.workingDirectory = metaFile.getAbsolutePath()
+                .substring(0, metaFile.getAbsolutePath().lastIndexOf('/')) + "/";
+    }
+
+    @Override
+    public void run() {
+        // get partitions from meta file
+        joinPartitions();
+    }
+
+    private void joinPartitions() {
+        int completed = 0;
+        updateProgress(completed);
+        var destinationFile = new File(workingDirectory + outputFileName);
+        try (
+                var metaFileStream = new BufferedReader(new FileReader(metaFile));
+                var destinationFileStream = new BufferedOutputStream(
+                        new FileOutputStream(destinationFile)
+                );
+        ) {
+            var line = "";
+            var lineCount = 0;
+            while ((line = metaFileStream.readLine()) != null) {
+                ++lineCount;
+                if (lineCount == FILE_NAME_LINE_NUMBER) {  // get file name from line 2
+                    this.outputFileName = line.split(":")[1];
+                }
+                if (lineCount > PARTITIONS_NAME_START_NUMBER) {  // read partitions name and size
+                    var parts = line.split(":");
+
+                    var partitionName = parts[0];
+                    var partitionSize = Long.parseLong(parts[1]);
+
+                    var partitionFile = new File(workingDirectory + partitionName);
+                    // check if current partition is in current directory
+                    if (!partitionFile.exists() || (partitionFile.length() != partitionSize))
+                        throw new PartitionException("Partition not found.");  // error if all partitions are not found
+
+                    // read file content from each partitions
+                    try (var partitionStream = new BufferedInputStream(new FileInputStream(partitionFile))) {
+                        var ripper = new ContentRipper(
+                                partitionStream,
+                                destinationFileStream,
+                                partitionSize
+                        );
+                        ripper.rip();
+                        updateProgress(++completed);  // update progress
+                    } catch (IOException e) {
+                        var partitionException = new PartitionException("Can not read " + partitionName);
+                        partitionException.initCause(e);
+                        throw partitionException;
+                    }
+                }
+            }
+            destinationFile.renameTo(new File(workingDirectory + outputFileName));
+        } catch (IOException e) {
+            var metaFileException = new MetaFileException();
+            metaFileException.initCause(e);
+            throw metaFileException;
+        }
+    }
+
+    public void addProgressListener(ProgressListener progressListener) {
+        this.progressListener = progressListener;
+    }
+
+    private void updateProgress(int joined) {
+        if (progressListener != null) progressListener.onProgressChanged(joined);
+    }
+
+    public interface ProgressListener {
+        void onProgressChanged(int joined);
+    }
+}
